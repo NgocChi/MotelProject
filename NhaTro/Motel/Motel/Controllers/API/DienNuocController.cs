6577f16aa857850df66f9af3f252145162be4c53
Thạch Minh Tâm <tamtm@runsystem.net>
2020-07-23 00:24:23 +0700

    save code

---
+++ NhaTro/Motel/Motel/Controllers/API/DienNuocController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Motel.Models.API.ElectrictyAndWaters;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Motel.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class DienNuocController : Controller
+    {
+        public DienNuocController()
+        {
+
+        }
+        Task<ElectrictyAndWaterResponse> GetListElectrictyAndWaterByTime([FromBody] ElectrictyAndWaterRequest request)
+        {
+            var response = new ElectrictyAndWaterResponse();
+            if (request == null || request.AccessToken == "OK")
+            {
+                response.Message = "Failed";
+                response.StatusCode = 1;
+            }
+        }
+    }
+}
---
+++ NhaTro/Motel/Motel/Models/API/ElectrictyAndWaters/ElectrictyAndWaterResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Motel.Models.API.Bases;
+using Newtonsoft.Json;
+
+namespace Motel.Models.API.ElectrictyAndWaters
+{
+    public class ElectrictyAndWaterRequest : RequestBase
+    {
+        [JsonProperty("TimeInput")]
+        public DateTime TimeInput { get; set; }
+
+        [JsonProperty("MaNhaTro")]
+        public int MaNhaTro { get; set; }
+    }
+
+    public class ElectrictyAndWaterResponse : ResponseBase
+    {
+        [JsonProperty("MaPhong")]
+        public int MaPhong { get; set; }
+
+        [JsonProperty("DaChotSo")]
+        public bool DaChotSo { get; set; }
+
+        [JsonProperty("ElectrictyAndWaterDtos")]
+        public IList<ElectrictyAndWaterDto> ElectrictyAndWaterDtos { get; set; }
+
+    }
+
+    public class ElectrictyAndWaterDto
+    {
+        [JsonProperty("MaPhong")]
+        public int MaPhong { get; set; }
+
+        [JsonProperty("TenPhong")]
+        public string TenPhong { get; set; }
+
+        [JsonProperty("ChiSoDienCu")]
+        public int ChiSoDienCu { get; set; }
+
+        [JsonProperty("ChiSoDienMoi")]
+        public int ChiSoDienMoi { get; set; }
+
+        [JsonProperty("ChiSoNuocCu")]
+        public int ChiSoNuocCu { get; set; }
+
+        [JsonProperty("ChiSoNuocMoi")]
+        public int ChiSoNuocMoi { get; set; }
+    }
+}
